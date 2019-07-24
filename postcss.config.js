c9e839f1fd1a895734b929c77d901ba1d36c02ac
Daniel Keane <keane.dp@gmail.com>
2019-07-24 15:45:26 -0500

    Optimizing for Production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     plugins: [
-        require('tailwindcss'),
-        require('autoprefixer')
+      require('tailwindcss'),
+      require('autoprefixer'),
+      process.env.BUILD_ENV === 'prod' && require('@fullhuman/postcss-purgecss')({
+        content: [
+          './resources/public/index.html',
+          './src/workation/core.cljs'
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      }),
+      process.env.BUILD_ENV === 'prod' && require('cssnano')
     ]
 }
